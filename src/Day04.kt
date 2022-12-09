d3dd57645ccf2014013581db8bb6cf87c638cd93
Hielke de Haan <hielkedehaan@gmail.com>
2022-12-09 20:39:38 +0100

    day 04

---
+++ src/Day04.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val input = input("Day04")
+    println("${part1(input)}")
+    println("${part2(input)}")
+}
+
+private fun part1(input: List<String>): Int =
+    input
+        .map(::split)
+        .count(::overlapCompletely)
+
+private fun part2(input: List<String>): Int =
+    input
+        .map(::split)
+        .count(::overlapPartly)
+
+private fun split(string: String): List<Int> =
+    string
+        .split(",", "-")
+        .map(String::toInt)
+
+private fun overlapCompletely(ranges: List<Int>): Boolean =
+    ranges[0] <= ranges[2] && ranges[1] >= ranges[3]
+            || ranges[0] >= ranges[2] && ranges[1] <= ranges[3]
+
+private fun overlapPartly(ranges: List<Int>): Boolean =
+    ranges[2] <= ranges[1] && ranges[1] <= ranges[3]
+            || ranges[0] <= ranges[3] && ranges[3] <= ranges[1]
